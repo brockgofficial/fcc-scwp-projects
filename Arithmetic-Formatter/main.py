aa194a174d9041415d5e195d118cf55529c0b4e6
Brock Giller <Brock.Giller@gmail.com>
2022-06-05 15:00:33 +1000

    changed workflow to run from project specific directory

--- Arithmetic-Formatter/main.py
+++ Arithmetic-Formatter/main.py
@@ -2,19 +2,5 @@
 import sys
 from pytest import main
 
-#Import in-house modules/packages
-from arithmetic_arranger import arithmetic_arranger
-
-#Custom tests for debugging
-# personal_tests = [
-#     (["32 + 698", "3801 - 2", "45 + 43", "123 + 49"], False),
-#     (['3801 - 2', '123 + 49'], False),
-#     (["320 + 698", "3801 - 384", "45 + 43", "32 + 457", "120 - 67"], True)
-# ]
-
-# for item in personal_tests:
-#     print(arithmetic_arranger(item[0], item[1]))
-#     print("\n")
-
 #Run unit tests automatically
 sys.exit(main())
